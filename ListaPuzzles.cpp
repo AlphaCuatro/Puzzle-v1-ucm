55dffd56eb84bc00eeb5fdbb8c80660735a7bbb9
AlphaCuatro <cuatr0@protonmail.com>
2021-05-30 12:48:07 +0200

    included declarartions in listaPuzzles.h

--- ListaPuzzles.cpp
+++ ListaPuzzles.cpp
@@ -1,2 +1,6 @@
 #include "ListaPuzzles.h"
 
+void inicializar(tPuzzlesReunidos& jr)
+{
+
+}
--- ListaPuzzles.h
+++ ListaPuzzles.h
@@ -6,7 +6,13 @@
 const int MAX_PUZZLES = 100;
 
 typedef tPuzzle* tListaPuzzles[MAX_PUZZLES];
+typedef tListaPuzzles tPuzzlesReunidos[2];
 
-
+void inicializar(tPuzzlesReunidos& jr);
+bool cargar(tPuzzlesReunidos& jr);
+void guardar(const tPuzzlesReunidos& listas);
+int elegirPuzzle(tListaPuzzles& lp);
+bool insertarOrdenado(tListaPuzzles& l, tPuzzle* p);
+bool buscar(const tListaPuzzles& lista, tPuzzle* puzzle, int& pos);
 
 #endif
